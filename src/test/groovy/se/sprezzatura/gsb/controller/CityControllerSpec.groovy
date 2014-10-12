37883a8d9bf9480eed7454627688e80110eb557c
Thomas Westin <thomas.westin@sprezzatura.se>
2014-10-12 17:46:14 +0200

    Added unit test

---
+++ src/test/groovy/se/sprezzatura/gsb/controller/CityControllerSpec.groovy
@@ -0,0 +1,11 @@
+package se.sprezzatura.gsb.controller
+
+import spock.lang.Specification
+
+class CityControllerSpec extends Specification {
+
+    void "testing spock works"() {
+        expect:
+        true
+    }
+}
